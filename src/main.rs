d9fc101bc6e84a6de55599d4a925e3a4f0d6380e
Marton Papp <morzzz007@gmail.com>
2021-09-24 14:16:08 +0200

    feat(app): print output to stdout CDP-1737

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use dotenv::dotenv;
 use std::env;
 use std::error;
 use std::fs;
-use std::fs::File;
-use std::io::prelude::*;
 use std::path::Path;
 
 mod database;
@@ -37,7 +35,7 @@ fn read_additional_typings() -> Option<String> {
     return Some(fs::read_to_string(filename).unwrap());
 }
 
-fn write_additional_typings_to_file(mut file: &File) -> Vec<String> {
+fn write_additional_typings_to_file(output: &mut String) -> Vec<String> {
     let additional_typings = read_additional_typings();
     match additional_typings {
         Some(additional_typings) => {
@@ -47,15 +45,15 @@ fn write_additional_typings_to_file(mut file: &File) -> Vec<String> {
                 typing_generator::Source::TomlHashMap(decoded),
                 &Vec::new(),
             );
-            file.write(generated_typings.string_value.as_bytes())
-                .unwrap();
+
+            output.push_str(&generated_typings.string_value);
             generated_typings.types
         }
         None => Vec::new(),
     }
 }
 
-fn write_database_typings_to_file(mut file: &File, additional_types: &Vec<String>) {
+fn write_database_typings_to_file(output: &mut String, additional_types: &Vec<String>) {
     let mut client = database::connect(get_database_configuration().unwrap()).unwrap();
 
     let tables: Vec<database::Table>;
@@ -65,30 +63,24 @@ fn write_database_typings_to_file(mut file: &File, additional_types: &Vec<String
     }
 
     for table in tables.iter() {
-        file.write(
-            typing_generator::generate_typing(
+        output.push_str(
+            &typing_generator::generate_typing(
                 typing_generator::Source::DatabaseTable(table),
                 additional_types,
             )
-            .string_value
-            .as_bytes(),
-        )
-        .unwrap();
+            .string_value,
+        );
     }
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     dotenv().ok();
 
-    let path = Path::new("types.d.ts");
-    let display = path.display();
+    let mut output: String = "".to_owned();
 
-    let file = match File::create(&path) {
-        Err(why) => panic!("Couldn't create {}: {}", display, why),
-        Ok(file) => file,
-    };
+    let additional_types = write_additional_typings_to_file(&mut output);
+    write_database_typings_to_file(&mut output, &additional_types);
 
-    let additional_types = write_additional_typings_to_file(&file);
-    write_database_typings_to_file(&file, &additional_types);
+    println!("{}", output);
     Ok(())
 }
